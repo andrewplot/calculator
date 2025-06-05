6a551d5a2aa4eba222eaf80f43220256c0bdb1ab
andrewplot <andrewplotplot@gmail.com>
2025-06-05 18:12:24 -0400

    incorporated logic

--- script.js
+++ script.js
@@ -1,28 +1,100 @@
-//wire buttons to numbers
-//parse buttons into string, add spaces on each side of operator
-//split at spaces
-//parse numbers, call appropriate operation's function
-//return answer
+//perhaps move typing to bottom display, and when pressing equals: have full eq on top, answer on bototm. then move ans to top when continuing operations
 
+//dom elements
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const clearButton = document.querySelector('[data-clear]');
 
+const currDisplay = document.querySelector('[data-curr]');
+const prevDisplay = document.querySelector('[data-prev]');
 
+//state
+let currOperand = '';
+let prevOperand = '';
+let operator = null;
 
+//functions
+function updateDisplay(){
+    currDisplay.textContent = currOperand;
+    if (operator !== ''){
+        prevDisplay.textContent = prevOperand;
+    } else {
+        prevDisplay.textContent = '';
+    }
+}
 
-const a = 0;
-const b = 0;
-const operator = Null;
+function appendNum(num){
+    if (num === '.' && currOperand.includes('.')) return;
+    currOperand += num;
+}
+
+function selectOperator(op){
+    if (currOperand === '') return; //exits if operator is first button
+    if (prevOperand !== ''){           //evalutes if chaining operations
+        evaluate();
+    }
+    operator = op;
+    prevOperand = currOperand;
+    currOperand = '';
+}
 
 function add(a, b){return a+b}
 function subtract(a, b){return a-b}
 function multiply(a, b){return a*b}
-function divide(a, b){return a/b}
+function divide(a, b){
+    if (b === 0){
+        return 'Undefined';
+    }
+        return a/b
+}
 
-function operate(a, b, operator){
-    switch (operator){
-        case '+': return add(a,b);
-        case '-': return subtract(a,b);
-        case '*': return multiply(a,b);
-        case '/': return divide(a,b);
+function evaluate(){
+    const a = parseFloat(prevOperand);
+    const b = parseFloat(currOperand);
+    let result;
+
+    if (isNaN(a) || isNaN(b)) return;
+    switch(operator){
+        case '+': result = add(a,b); break;
+        case '-': result = subtract(a,b); break;
+        case '*': result = multiply(a,b); break;
+        case '/': result = divide(a,b); break;
         default: return Error;
     }
+
+    currOperand = result.toString();
+    prevOperand = `${a} ${operator} ${b} = `;
+    operator = null;
 }
+
+function clearDisplay(){
+    currOperand = '';
+    prevOperand = '';
+    operator = null;
+}
+
+//event listeners
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    appendNum(button.getAttribute('data-number'));
+    updateDisplay();
+  });
+});
+
+operationButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    selectOperator(button.getAttribute('data-operation'));
+    updateDisplay();
+  });
+});
+
+equalsButton.addEventListener('click', () => {
+  evaluate();
+  updateDisplay();
+});
+
+clearButton.addEventListener('click', () => {
+  clearDisplay();
+  updateDisplay();
+});
